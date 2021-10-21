f21d425c4eee1659900cdc23a95195a0420fcc65
Vdovkin Oleksandr <a.a.vdovkin@gmail.com>
2021-10-21 18:17:49 +0300

    update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+import HastPage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+      <Route exact path='/' component={HomePage} />
+      </Switch>
+      
     </div>
   );
 }
